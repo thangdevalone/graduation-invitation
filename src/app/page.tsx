1bd1ddc0d034a6ef45613415963b1ed843517348
Thắng Nguyễn <thangdevalone@gmail.com>
2025-06-26 03:40:38 +0700

    ✨ paralax image list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { CardIntroduce } from "@/components/card-introduce";
+import { Hero } from "@/components/hero";
 import { Header } from "@/components/layouts/header";
+import { ParalaxImageGraduation } from "@/components/paralax-image-graduation";
+import { TimelineSection } from "@/components/time-line";
 
 export default function Home() {
   return (
     <div className="relative w-full">
+      {/* <Header /> */}
       <Header />
+      <Hero />
+      <CardIntroduce />
+      <TimelineSection />
+      <ParalaxImageGraduation />
     </div>
   );
 }
