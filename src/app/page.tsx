b98fcb4020b63eedc5807b250a270f7cb263954c
Thắng Nguyễn <thangdevalone@gmail.com>
2025-06-26 03:54:28 +0700

    ⚡️ update footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { CardIntroduce } from "@/components/card-introduce";
 import { Hero } from "@/components/hero";
+import { Footer } from "@/components/layouts/footer";
 import { Header } from "@/components/layouts/header";
 import { ParalaxImageGraduation } from "@/components/paralax-image-graduation";
 import { TimelineSection } from "@/components/time-line";
@@ -13,6 +14,7 @@ export default function Home() {
       <CardIntroduce />
       <TimelineSection />
       <ParalaxImageGraduation />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layouts/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <div className="text-center text-sm text-neutral-500 py-10 border-t">
+      Landing page by ThangDev @ 2025. All rights reserved.
+    </div>
+  );
+}
